4def56e3ac4c4082be15b00e33e2a3b68dcd7826
Bogdan Covrig <bogcov@gmail.com>
2023-05-10 12:00:49 +0200

    feature: Add list on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h2 className="mb-3 text-2xl font-semibold">Hey sky</h2>
+      <ul className="list-disc">
+        <li>
+          <Link href="/london">London</Link>
+        </li>
+        <li>
+          <Link href="/rome">Rome</Link>
+        </li>
+        <li>
+          <Link href="/piatra-neamt">Piatra</Link>
+        </li>
+      </ul>
     </main>
   );
 }
